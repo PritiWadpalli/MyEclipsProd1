530c5855f299279ac9f59bb409bc3cb4b0639bdb
Priti <shriramwar.priti@gmail.com>
2022-02-10 17:17:39 +0530

    first test case is updated

---
+++ src/test/java/com/testcases/MyTest1.java
@@ -0,0 +1,13 @@
+package com.testcases;
+
+import org.testng.annotations.Test;
+
+public class MyTest1 {
+
+	@Test
+	public void funTest()
+	{
+		System.out.println("My test case if not yet desinged");
+	}
+	
+}
